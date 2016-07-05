3f9513aa525a885718deec9f63179b5ada74f1a9
Marcio Nicolau <marcionicolau@me.com>
2016-07-04 23:14:02 -0300

    Update _buil.sh to use Rscript

--- _build.sh
+++ _build.sh
@@ -1,7 +1,5 @@
-#!/usr/bin/env Rscript
+#!/bin/sh
 
-require(bookdown)
-
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
-bookdown::render_book("index.Rmd", "bookdown::pdf_book")
-bookdown::render_book("index.Rmd", "bookdown::epub_book")
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')" 
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')" 
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')" 
