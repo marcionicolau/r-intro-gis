23373c6a4ee38e52cf109416375406b1be2b5d5e
Marcio Nicolau <marcionicolau@me.com>
2016-07-04 22:24:39 -0300

    Update _buil.sh

--- _build.sh
+++ _build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env Rscript
 
+require(bookdown)
+
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+bookdown::render_book("index.Rmd", "bookdown::epub_book")
